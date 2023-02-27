fdb09ee6dfd6a7cf8659773963ad3b06db474b30
Aatishcodes <118838033+Aatishcodes@users.noreply.github.com>
2023-02-27 17:42:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+
+
+var a = 10;
+var b = 5;
+
+function showdata() {
+    var add = a + b;
+    console.log(add)
+    console.log(full_name);
+}
